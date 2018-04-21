c691abb6b6c90ad32b4025f8148121d87c45c189
Gianni Poutanen <g.poutanen@uqconnec.edu.au>
2018-04-21 15:01:05 +1000

    model update

--- Model.py
+++ Model.py
@@ -1,6 +1,5 @@
-
-class User
-    def __init__(self, id, name, department)
+class User:
+    def __init__ (self, id, name, department):
         self.level = 1
         self.currentXp = 0
         self.nextLevel = 50
@@ -10,55 +9,50 @@ class User
         self.department = department
         self.achievements = instantiateAchievements
 
-    def instantiateAchievements
+    def instantiateAchievements():
         array = []
         array + Milestone("Weakly Streak", 10).generateMilestones(10, 5)
-        print array
         return array
 
-    def giveXp(x)
+    def giveXp (x):
         self.currentXp += x
-        if self.currentXp >= self.nextLevel
+        if self.currentXp >= self.nextLevel:
             level += 1
             self.nextlevel = self.nextLevel * (1+log2(level))
 
 
-    def getValues(param)
+    def getValues (param):
         return self.currentXp
 
-    def endStreak(x)
+    def endStreak (x):
         self.attendanceStreak = 0
     
-    def incrementStreak
+    def incrementStreak():
         self.attendanceStreak += 1
 
-class Achievement
-    def __init__(self, name, xp)
+class Achievement():
+    def __init__(self, name, xp):
         self.xp = xp
         self.name = name
         self.goal = 50
         self.achieved = false
 
-    def compareValues(user)
-        return user.getValues(name).equal?(goal)
+    def compareValues(user):
+        return user.getValues(name).equal(goal)
 
-    def giveAchievement(user)
-        user.achievements.push(@name)
+    def giveAchievement(user):
+        user.achievements.push(name)
         user.giveXp(xp)
         self.achieved = true
 
-class Milestone(Achievement)
-    def __init__
+class Milestone(Achievement):
+    def __init__():
         super(self, name, xp)
 
-    def generateMilestones(amount, factor)
+    def generateMilestones(amount, factor):
         array = []
         i = 0
-        while i < amount
+        while i < amount:
             array.push(Achievement(self.name + " " + i, i * factor))
             i += 1
         return array
-
-
-#Test Space
-
